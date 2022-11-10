2fa6c5a17e196a334e641e027ae45e1644a03518
pecres <pjcrespo16@gmail.com>
2022-11-10 17:32:00 -0300

    Registro y tabla realizados

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.agregarUsuario, name='agregarUsuario'),
     path('agregarUsuario', views.agregarUsuario, name='agregarUsuario'),
-    path('tabla', views.crearTabla, name='crearTabla'),
+    path('agregarUsuario.html', views.agregarUsuario, name='agregarUsuario'),
+    path('table.html', views.crearTabla, name='crearTabla'),
 ]
